1db9460189e82c19dc93ea4b22828303032f3a03
jinwood93 <wood@codberg.com>
2025-12-02 10:22:59 +0900

    category menu

--- app/page.tsx
+++ app/page.tsx
@@ -2,64 +2,32 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="min-h-screen">
+      <div className="w-full flex justify-center">
+        <div className="backdrop-blur bg-white/30 w-fit flex items-center justify-center gap-4 p-1 rounded-[24px] shadow-md">
+          <div className="transition duration-300 ease-in-out hover:bg-[#D1F8EF] text-gray-500 font-semibold  cursor-pointer rounded-[24px] px-4 py-2">
+            핵심역량
+          </div>
+          <div className="transition duration-300 ease-in-out hover:bg-[#D1F8EF] text-gray-500 font-semibold cursor-pointer rounded-[24px] px-4 py-2">
+            기술
+          </div>
+          <div className="transition duration-300 ease-in-out hover:bg-[#D1F8EF] text-gray-500 font-semibold cursor-pointer rounded-[24px] px-4 py-2">
+            경력
+          </div>
+          <div className="transition duration-300 ease-in-out hover:bg-[#D1F8EF] text-gray-500 font-semibold cursor-pointer rounded-[24px] px-4 py-2">
+            프로젝트
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
+      <div>짧은 소개</div>
+      <div>이력서 다운로드</div>
+      <div>카테고리 목록(고정)</div>
+      <div>핵심역량</div>
+      <div>기술스택및도구</div>
+      <div>경력</div>
+      <div>프로젝트 상세</div>
+      <div>etc</div>
+      <div>contact me</div>
     </div>
   );
 }
